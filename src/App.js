161521496ef6e5b1cf0ae2dbb37c5d83fd8754db
Leila Gonzalez Tamburri <leila.gonzalezt@gmail.com>
2023-05-01 14:27:40 -0300

    Agregando 1 y 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
+
+
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenido'}/>
+      <ItemCount initial={1} stock= {10} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)} />
     </div>
   );
 }
---
+++ src/asyncMock.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: '1',
+        name: 'Hogwarts Legacy',
+        price: '50 USD',
+        category: 'RPG',
+        img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/990080/header.jpg?t=1680647016',
+        description: 'Descripcion del Hogwarts Legacy'
+    },
+    { id: '2', name: 'Resident Evil 4', price: '45 USD', category: 'singlePlayer', img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/2050650/header.jpg?t=1680840402', description: 'Descripcion del Resident Evil 4'},
+    {id: '3', name: 'Elden Ring', price: '20 USD', category: 'RPG', img: 'https://gaming-cdn.com/images/products/4824/616x353/elden-ring-pc-juego-steam-europe-cover.jpg?v=1650985585', description: 'Descripcion del Elden Ring'}
+]
+
+export const getProducts = () => {
+    return new Promise ((resolve) => {
+        setTimeout(()=> {
+            resolve(products)
+        }, 5000)
+    })
+}
